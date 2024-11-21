5de1557e43a816fc3b0b9e31fec50cee4a000d16
Andrii <andreyjgavrelenko@gmail.com>
2024-11-21 05:42:58 +0200

    виправлено помилку з записуванням

--- Lab5/main.py
+++ Lab5/main.py
@@ -1,23 +1,22 @@
 import time
 
-file = open("information.txt", "w+")
-Choice = input("Clear file? Yes or No")
-if Choice == "Yes" or Choice == "yes":
-    file.truncate(0)
+
 while True:
-    Name = input("Your Name:")
-    Surname = input("Your Surname:")
-    Age = input("You Age:")
+
+    with open("information.txt", "a") as file:
+        Name = input("Your Name:")
+        Surname = input("Your Surname:")
+        Age = input("You Age:")
+        file.write(f"{Name} | {Surname} | {Age}\n")
+
+
     Choice = input("Continue? Yes or No")
-    file_content = file.read()
-    file.write(file_content + f"{Name} {Surname} {Age}\n")
     if Choice == "Yes" or Choice == "yes":
         continue
     elif Choice == "No" or Choice == "no":
         print("Closing...")
         time.sleep(1)
-        exit()
-    else:
+
         break
 
 
